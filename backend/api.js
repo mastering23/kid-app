ba1a4db7d97e744cd99b68ae3ba54787845eb6dd
Berihuete Nunez, Engel <mastering23@gmail.com>
2026-07-12 02:26:00 -0400

    updating api.js

--- backend/api.js
+++ backend/api.js
@@ -7,6 +7,12 @@ app.get("/", (req, res) => {
   res.send("<h1>Hello</h1>");
 });
 
+app.get("/", (req, res) => {
+  res.send("<h1>Hello</h1>");
+});
+
+
+
 app.listen(PORT, () => {
   console.log(`Server listening on http://localhost:${PORT}`);
 });
